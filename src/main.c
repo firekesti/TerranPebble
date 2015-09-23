50ef56fbb1f6310d9762403a5fe5fd295691f487
Kate <firekesti@users.noreply.github.com>
2015-09-23 15:57:42 -0400

    add color!

--- src/main.c
+++ src/main.c
@@ -7,8 +7,6 @@ static TextLayer *s_battery_layer;
 static TextLayer *s_bluetooth_layer;
 static GFont s_time_font;
 static GFont s_date_font;
-static GColor background_color = GColorBlack;
-static GColor text_color = GColorWhite;
 // Vibe pattern: ON in ms then OFF etc
 static uint32_t const disconnect_segments[] = { 100, 100, 400, 100, 100 };
 
@@ -78,77 +76,80 @@ static void handle_bluetooth(bool connected) {
 }
 
 static void main_window_load(Window *window) {
-  window_set_background_color(window, background_color);
-  
-  // Create battery TextLayer
-  // top: 10
-  // paddingTop: 10
-  // height: 24
-  // bottom: 34
-  s_battery_layer = text_layer_create(GRect(0, 10, 144, 24));
-  text_layer_set_text_color(s_battery_layer, text_color);
-  text_layer_set_background_color(s_battery_layer, background_color);
-  text_layer_set_text(s_battery_layer, "100%");
+    GColor background_color = COLOR_FALLBACK(GColorDukeBlue, GColorBlack);
+    GColor text_color = COLOR_FALLBACK(GColorIcterine, GColorWhite);
+    
+    window_set_background_color(window, background_color);
+
+    // Create battery TextLayer
+    // top: 10
+    // paddingTop: 10
+    // height: 24
+    // bottom: 34
+    s_battery_layer = text_layer_create(GRect(0, 10, 144, 24));
+    text_layer_set_text_color(s_battery_layer, text_color);
+    text_layer_set_background_color(s_battery_layer, background_color);
+    text_layer_set_text(s_battery_layer, "100%");
 
 
-  // Create time TextLayer
-  // top: 40
-  // paddingTop: 6
-  // height: 48
-  // bottom: 88
-  s_time_layer = text_layer_create(GRect(0, 40, 144, 48));
-  text_layer_set_background_color(s_time_layer, background_color);
-  text_layer_set_text_color(s_time_layer, text_color);
-  text_layer_set_text(s_time_layer, "00:00");
-  
-  // Create date TextLayer
-  // top: 106
-  // paddingTop: 18
-  // height: 24
-  // bottom: 130
-  s_date_layer = text_layer_create(GRect(0, 106, 144, 24));
-  text_layer_set_background_color(s_date_layer, background_color);
-  text_layer_set_text_color(s_date_layer, text_color);
-  text_layer_set_text(s_date_layer, "Day Mon Dt");
-  
-  // Create bluetooth TextLayer
-  // top: 136
-  // paddingTop: 12
-  // height: 24
-  // bottom: 160
-  // paddingBottom: 6
-  s_bluetooth_layer = text_layer_create(GRect(0, 136, 144, 24));
-  text_layer_set_text_color(s_bluetooth_layer, text_color);
-  text_layer_set_background_color(s_bluetooth_layer, background_color);
-  text_layer_set_text(s_bluetooth_layer, "<B>");
-  
-  // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TERRAN_48));
-  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TERRAN_24));
+    // Create time TextLayer
+    // top: 40
+    // paddingTop: 6
+    // height: 48
+    // bottom: 88
+    s_time_layer = text_layer_create(GRect(0, 40, 144, 48));
+    text_layer_set_background_color(s_time_layer, background_color);
+    text_layer_set_text_color(s_time_layer, text_color);
+    text_layer_set_text(s_time_layer, "00:00");
 
-  // Apply to TextLayers
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_font(s_date_layer, s_date_font);
-  text_layer_set_font(s_battery_layer, s_date_font);
-  text_layer_set_font(s_bluetooth_layer, s_date_font);
-  
-  // Improve the layout to be more like a watchface
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
-  text_layer_set_text_alignment(s_bluetooth_layer, GTextAlignmentCenter);
+    // Create date TextLayer
+    // top: 106
+    // paddingTop: 18
+    // height: 24
+    // bottom: 130
+    s_date_layer = text_layer_create(GRect(0, 106, 144, 24));
+    text_layer_set_background_color(s_date_layer, background_color);
+    text_layer_set_text_color(s_date_layer, text_color);
+    text_layer_set_text(s_date_layer, "Day Mon Dt");
 
-  // Add them as child layers to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_battery_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_bluetooth_layer));
-  
-  // Make sure the right info is displayed from the start
-  update_time();
-  update_date();
-  handle_battery(battery_state_service_peek());
-  handle_bluetooth_init(bluetooth_connection_service_peek());
+    // Create bluetooth TextLayer
+    // top: 136
+    // paddingTop: 12
+    // height: 24
+    // bottom: 160
+    // paddingBottom: 6
+    s_bluetooth_layer = text_layer_create(GRect(0, 136, 144, 24));
+    text_layer_set_text_color(s_bluetooth_layer, text_color);
+    text_layer_set_background_color(s_bluetooth_layer, background_color);
+    text_layer_set_text(s_bluetooth_layer, "<B>");
+
+    // Create GFont
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TERRAN_48));
+    s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TERRAN_24));
+
+    // Apply to TextLayers
+    text_layer_set_font(s_time_layer, s_time_font);
+    text_layer_set_font(s_date_layer, s_date_font);
+    text_layer_set_font(s_battery_layer, s_date_font);
+    text_layer_set_font(s_bluetooth_layer, s_date_font);
+
+    // Improve the layout to be more like a watchface
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+    text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+    text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
+    text_layer_set_text_alignment(s_bluetooth_layer, GTextAlignmentCenter);
+
+    // Add them as child layers to the Window's root layer
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_battery_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_bluetooth_layer));
+
+    // Make sure the right info is displayed from the start
+    update_time();
+    update_date();
+    handle_battery(battery_state_service_peek());
+    handle_bluetooth_init(bluetooth_connection_service_peek());
 }
 
 static void main_window_unload(Window *window) {
